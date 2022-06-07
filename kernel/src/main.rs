f2047e08aa09d5205d95a9fc5ea2373150ca5bc3
Kakeru Ishii <kyasbal1994@gmail.com>
2022-06-08 00:28:23 +0900

    Implement println! macro

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,15 +5,43 @@
 mod screen;
 
 use core::arch::asm;
-use core::slice;
+use core::{fmt, slice};
+use core::fmt::Arguments;
 use shared::framebuffer;
 use crate::screen::{Renderable, Screen};
 
+static mut CONSOLE: Option<Screen> = Option::None;
+
+#[derive(Debug)]
+pub struct ConsoleWrite;
+
+impl fmt::Write for ConsoleWrite {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        unsafe { CONSOLE.as_mut().unwrap().write_string(0, 0, s, [0, 0, 0]); }
+        Ok(())
+    }
+}
+#[allow(unused_macros)]
+macro_rules! println {
+    ($($t:tt)*)=>{{
+        use core::fmt::Write;
+        writeln!(ConsoleWrite, $($t)*).unwrap();
+    }}
+}
+
+macro_rules! clear_screen {
+    ()=>{
+    unsafe{CONSOLE.as_mut().unwrap().clear([255,255,255])};
+    }
+}
+
 #[no_mangle]
-pub extern "sysv64" fn kernel_main(fb:&framebuffer::FrameBuffer) {
-    let mut screen:Screen = (*fb).into();
-    screen.clear([255,255,255]);
-    screen.write_string(100,30,"HELLO WORLD!!",[255,0,255]);
+pub extern "sysv64" fn kernel_main(fb: &framebuffer::FrameBuffer) {
+    unsafe {
+        CONSOLE = Some((*fb).into());
+    }
+    clear_screen!();
+    println!("hello world");
     loop {
         unsafe { asm!("hlt") }
     }
